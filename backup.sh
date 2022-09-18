da43cda55ca0bc182637062a65773ddcb2fccaf8
Insider <insider@loopback>
2022-09-18 23:48:08 +0300

    Upgrade backup script

--- backup.sh
+++ backup.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 filename="gfs_$(date +"%F %T").zip"
+yandexCloud="/media/yandexCloud/backups/"
+mailCloud="/media/mailCloud/backups/"
+orico="/media/orico/backups_development/"
 
+echo "Create archive $filename"
 git archive --output=../backup/"$filename" HEAD
 
-cp ../backup/"$filename" /media/yandexCloud/backups/
-cp ../backup/"$filename" /media/mailCloud/backups/
-cp ../backup/"$filename" /media/orico/backups_development/
+echo "Copy to yandex cloud $yandexCloud"
+cp ../backup/"$filename" $yandexCloud
 
+echo "Copy to mail cloud $mailCloud"
+cp ../backup/"$filename" $mailCloud
 
+echo "Copy to orico $orico"
+cp ../backup/"$filename" $orico
 
+echo "Done"
